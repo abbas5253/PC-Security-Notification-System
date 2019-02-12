69dad710f36f1ddb6a1e1f0784ae8de6c3d753d3
Abbas Ismail <abbas.tel2342@gmail.com>
2019-02-12 21:52:11 +0530

    added comments to the face_train.py file

--- Face_train.py
+++ Face_train.py
@@ -3,8 +3,8 @@ import numpy as np
 from PIL import Image
 import cv2
 import pickle
- 
- 
+
+ #this is .py for training the model using images provided by admins
 # Loading the cascades
 face_cascade = cv2.CascadeClassifier(r'haarcascade_frontalface_alt.xml')
 
@@ -12,7 +12,7 @@ BASE_DIR = os.getcwd()
 image_dir = os.path.dirname(r'.\images')
 
 
-recognizer  = cv2.face.LBPHFaceRecognizer_create() 
+recognizer  = cv2.face.LBPHFaceRecognizer_create()
 
 
 current_id = 0
@@ -31,12 +31,12 @@ for  root,dirs,files in os.walk(image_dir):
             else:
                 label_ids[label] = current_id
                 current_id+=1
-               
+
             id_ = label_ids[label]
             pil_image = Image.open(path) .convert("L")#give inmage on that path and convert it into gray
             image_array = np.array(pil_image,"uint8")
             faces = face_cascade.detectMultiScale(image_array, 1.3, 5)
-        
+
             for (x,y,w,h) in faces:
                  roi = image_array[y:y+h,x:x+w]
                  x_train.append(roi)
@@ -45,8 +45,8 @@ for  root,dirs,files in os.walk(image_dir):
 
 with open("label.pickle","wb") as f:
     pickle.dump(label_ids,f)   #creates file with the labels of admin and non-admin
-                  
-    
-    
+
+
+
 recognizer.train(x_train,np.array(y_labels))      #training of using local binary histogram pattern recognition
 recognizer.save("trainner.yml")
